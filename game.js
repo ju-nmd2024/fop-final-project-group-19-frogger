bfd69c8b5745fd2ac457f24c92aafc6708dd7fac
lisa22np <erli22np@student.ju.se>
2024-11-22 15:25:37 +0100

    created car class

--- game.js
+++ game.js
@@ -2,6 +2,40 @@ function setup() {
   createCanvas(800, 600);
 }
 
+let speed = 5;
+
 function draw() {
-  background(255, 140, 0);
+  background(50, 50, 50);
+}
+
+class Car {
+  constructor(x, y, lenght, height) {
+    this.x = x;
+    this.y = y;
+    this.lenght = lenght;
+    this.height = height;
+  }
+  draw() {
+    push();
+    translate(0, 0);
+    rect(this.x, this.y, this.lenght, this.height);
+    pop();
+
+    if (this.x <= 800) {
+      this.x = this.x + 1 * speed;
+    } else if (this.x > 800) {
+      this.x = 0 - this.lenght;
+    }
+  }
+}
+
+const firstCar = new Car(50, 100, 100, 50);
+const secondCar = new Car(0, 300, 100, 50);
+const thirdCar = new Car(150, 300, 100, 50);
+
+function draw() {
+  clear();
+  firstCar.draw();
+  secondCar.draw();
+  thirdCar.draw();
 }
