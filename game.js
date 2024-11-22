91a28574da0488890f3a9709e03757578c7834d4
lisa22np <erli22np@student.ju.se>
2024-11-22 16:24:38 +0100

    park

--- game.js
+++ game.js
@@ -3,9 +3,42 @@ function setup() {
 }
 
 let speed = 5;
+let gameState = "park";
 
-function draw() {
-  background(50, 50, 50);
+function parkScreen() {
+  background(66, 184, 61);
+  // Grass
+  push();
+  fill(47, 138, 43);
+  noStroke();
+  rect(0, 0, 800, 100);
+  rect(0, 500, 800, 100);
+  rect(0, 260, 800, 80);
+  pop();
+
+  //water
+  push();
+  noStroke();
+  fill(129, 218, 240);
+  rect(0, 180, 800, 80);
+  pop();
+
+  //path
+  push();
+  noStroke();
+  fill(97, 92, 88, 200);
+  rect(0, 110, 800, 60);
+
+  rect(0, 350, 800, 60);
+
+  rect(0, 430, 800, 60);
+
+  fill(97, 92, 88, 200);
+  rect(0, 115, 800, 50);
+  rect(0, 355, 800, 50);
+  rect(0, 435, 800, 50);
+
+  pop();
 }
 
 class Car {
@@ -34,8 +67,19 @@ const secondCar = new Car(0, 300, 100, 50);
 const thirdCar = new Car(150, 300, 100, 50);
 
 function draw() {
-  clear();
-  firstCar.draw();
-  secondCar.draw();
-  thirdCar.draw();
+  if (gameState === "start") {
+    background(227, 152, 173);
+  } else if (gameState === "park") {
+    parkScreen();
+
+    //firstCar.draw();
+    //secondCar.draw();
+    //thirdCar.draw();
+  } else if (gameState === "street") {
+    background(50, 50, 50);
+  } else if (gameState === "brickWall") {
+    background(186, 94, 63);
+  } else if (gameState === "end") {
+    background(115, 65, 125);
+  }
 }
