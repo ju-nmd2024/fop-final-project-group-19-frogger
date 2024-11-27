92dd3eb3e7a1ba817519571b73069fe9be351c5c
lisa22np <erli22np@student.ju.se>
2024-11-27 10:27:22 +0100

    street view

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(800, 600);
 }
 
-let gameState = "park";
+let gameState = "street";
 let gameLives = 3;
 
 function parkScreen() {
@@ -41,9 +41,38 @@ function parkScreen() {
   pop();
 }
 
+function streetScreen() {
+  push();
+  background(50, 50, 50);
+
+  noStroke();
+  fill(100, 100, 100);
+  rect(0, 0, 800, 100);
+  rect(0, 500, 800, 100);
+  rect(0, 260, 800, 80);
+
+  fill(220, 220, 220, 200);
+  rect(0, 105, 800, 5);
+  rect(0, 175, 800, 5);
+  rect(0, 250, 800, 5);
+
+  rect(0, 490, 800, 5);
+  rect(0, 420, 800, 5);
+  rect(0, 345, 800, 5);
+
+  fill(145, 167, 186, 150);
+  ellipse(60, 220, 200, 50);
+  ellipse(170, 390, 150, 50);
+  ellipse(350, 385, 200, 60);
+  ellipse(350, 385, 100, 50);
+  ellipse(500, 380, 150, 50);
+  pop();
+}
+
 class Car {
-  constructor(x, r, g, b, speed) {
+  constructor(x, y, r, g, b, speed) {
     this.x = x;
+    this.y = y;
     this.r = r;
     this.g = g;
     this.b = b;
@@ -54,15 +83,16 @@ class Car {
     translate(0, 0);
     fill(this.r - 50, this.g - 50, this.b - 50);
     noStroke();
-    rect(this.x, 435, 100, 50);
+    rect(this.x, this.y, 100, 50);
     fill(this.r, this.g, this.b);
-    rect(this.x + 20, 440, 60, 40);
+    rect(this.x + 20, this.y + 5, 60, 40);
     pop();
-
+  }
+  update() {
     if (gameState === "park") {
-      this.speed = 0;
+      this.speed = 4;
     } else if (gameState === "street") {
-      this.speed = 6;
+      this.speed = 0;
     } else if (gameState === "wall") {
       this.speed = 8;
     }
@@ -75,30 +105,109 @@ class Car {
   }
 }
 
-const car1 = new Car(0, 255, 255, 0);
-const car2 = new Car(450, 0, 255, 255);
-const car3 = new Car(600, 255, 0, 255);
+/*class CarBack extends Car {
+  constructor(x, y, r, g, b, speed){
+    super(x, y, r, g , b, speed);
+  }
 
-let cars = [car1, car2, car3];
+  update(){
+    if (gameState === "park") {
+      this.speed = 4;
+    }  else if (gameState === "street") {
+      this.speed = 6;
+    } else if (gameState === "wall") {
+      this.speed = 8;
+    }
+
+    if (this.x <= 800) {
+      this.x = this.x - this.speed;
+    } else if (this.x > 800) {
+      this.x = 800 + 100;
+    }
+  }
+}
+*/
+class CarBack {
+  constructor(x, y, r, g, b, speed) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.speed = speed;
+  }
+  draw() {
+    push();
+    translate(0, 0);
+    fill(this.r - 50, this.g - 50, this.b - 50);
+    noStroke();
+    rect(this.x, this.y, 100, 50);
+    fill(this.r, this.g, this.b);
+    rect(this.x + 20, this.y + 5, 60, 40);
+    pop();
+  }
+  update() {
+    if (gameState === "park") {
+      this.speed = 4;
+    } else if (gameState === "street") {
+      this.speed = 6;
+    } else if (gameState === "wall") {
+      this.speed = 8;
+    }
+
+    if (this.x > -100) {
+      this.x = this.x - this.speed;
+    } else if (this.x <= -100) {
+      this.x = 800 + 100;
+    }
+  }
+}
+
+const car1a = new Car(0, 435, 255, 0, 255, 1);
+const car1b = new Car(200, 435, 0, 255, 255, 1);
+const car1c = new Car(400, 435, 255, 255, 0, 1);
+
+const car2b = new CarBack(300, 355, 255, 255, 255, 1);
+
+let cars = [car1a, car1b, car1c];
 
 function draw() {
   if (gameState === "start") {
     background(227, 152, 173);
   } else if (gameState === "park") {
     parkScreen();
-    car1.draw();
-    car2.draw();
-    car3.draw();
+    car1a.draw();
+    car1a.update();
+
+    car1b.draw();
+    car1b.update();
+
+    car1c.draw();
+    car1c.update();
+
+    car2b.draw();
+    car2b.update();
   } else if (gameState === "street") {
-    background(50, 50, 50);
-    car1.draw();
-    car2.draw();
-    car3.draw();
+    streetScreen();
+
+    car1a.draw();
+    car1a.update();
+
+    car1b.draw();
+    car1b.update();
+
+    car1c.draw();
+    car1c.update();
   } else if (gameState === "wall") {
     background(186, 94, 63);
-    car1.draw();
-    car2.draw();
-    car3.draw();
+    car1a.draw();
+    car1a.update();
+
+    car1b.draw();
+    car1b.update();
+
+    car1c.draw();
+    car1c.update();
   } else if (gameState === "end") {
     background(115, 65, 125);
   }
